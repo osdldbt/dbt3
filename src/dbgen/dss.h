26870deb9d850de03ffbbbfb1dd445166586e7be
Mark Wong <mark@2ndQuadrant.com>
2014-09-04 15:21:43 -0700

    Add postgres specific macro for data generation

--- src/dbgen/dss.h
+++ src/dbgen/dss.h
@@ -479,7 +479,11 @@ int dbg_print(int dt, FILE *tgt, void *data, int len, int eol);
 #define PR_MONEY(f, str) 		dbg_print(DT_MONEY, f, (void *)str, 0, 1)
 #define PR_CHR(f, str)	 		dbg_print(DT_CHR, f, (void *)str, 0, 1)
 #define  PR_STRT(fp)   /* any line prep for a record goes here */
+#ifdef PGSQL
+#define  PR_END(fp)    {fseek(fp, -1, SEEK_CUR); fprintf(fp, "\n");}
+#else
 #define  PR_END(fp)    fprintf(fp, "\n")   /* finish the record here */
+#endif /* PGSQL */
 #ifdef MDY_DATE
 #define  PR_DATE(tgt, yr, mn, dy)	\
    sprintf(tgt, "%02d-%02d-19%02d", mn, dy, yr)
