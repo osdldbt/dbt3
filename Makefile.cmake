fb1865ad2d93d4e31a6351251864a69d977b3c3d
Mark Wong <markwkm@gmail.com>
2023-05-03 18:59:34 -0700

    Add debug and release rules to Makefile

--- Makefile.cmake
+++ Makefile.cmake
@@ -1,7 +1,11 @@
-.PHONY: clean default package
+.PHONY: clean debug default package release
 
 default:
 	@echo "targets: clean, package"
+
+debug:
+	cmake -H. -Bbuilds/debug -DCMAKE_BUILD_TYPE=Debug
+	cd builds/debug && make
 
 clean:
 	-rm -rf builds
@@ -10,3 +14,7 @@ package:
 	git checkout-index --prefix=builds/ -a
 	cmake -Hbuilds -Bbuilds
 	cd builds && make package_source
+
+release:
+	cmake -H. -Bbuilds/release
+	cd builds/release && make
